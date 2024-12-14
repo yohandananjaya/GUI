09b493fc7f8685f431fb002b6ad8b3129f87d805
yohandananjaya <ydananjaya47@gmail.com>
2024-12-15 00:00:04 +0530

    Update script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+const wrapper = document.querySelector('.wrapper');
+const loginLink = document.querySelector('.login-link');
+const btnPopup = document.querySelector('.btnLogin-popup');
+const iconClose = document.querySelector('.icon-close');
+
+
+
+btnPopup.addEventListener('click', ()=> {
+    wrapper.classList.add('active-popup');
+});
+iconClose.addEventListener('click', ()=> {
+    wrapper.classList.remove('active-popup');
+});
